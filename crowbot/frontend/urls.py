6f5fdfb5710aeffbd55e85b15df83ff4f661af69
Daniel Lisø <daniel.lisoe@gmail.com>
2017-02-15 13:34:41 +0100

    Fix routes

--- crowbot/frontend/urls.py
+++ crowbot/frontend/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'frontend'
+
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^new_post', views.new_post, name='new_post'),
+    url(r'^submit', views.submit, name='submit'),
+    url(r'', views.index, name='index'),
 ]
