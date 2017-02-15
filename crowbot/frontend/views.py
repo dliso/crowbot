e6c9ad98aa2da20f5db48a94646afd2e874bc7bc
Daniel Lisø <daniel.lisoe@gmail.com>
2017-02-15 11:27:41 +0100

    List user queries on main page

--- crowbot/frontend/views.py
+++ crowbot/frontend/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import UserQuery as UQ
+
 # Create your views here.
 
 def index(request):
-    return render(request, 'frontend/index.html')
+    queries = UQ.objects.all()
+    return render(request, 'frontend/index.html', {'queries': queries})
