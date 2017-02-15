f989b50a20f934453fbcf813989ba6472c9e3d8b
Daniel Lisø <daniel.lisoe@gmail.com>
2017-02-15 11:22:16 +0100

    Add a UserQuery model

--- crowbot/frontend/models.py
+++ crowbot/frontend/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class UserQuery(models.Model):
+    query_text = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.query_text[:50]
