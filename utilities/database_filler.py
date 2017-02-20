5cc9bc7b8caf4aade73520876affeaab6c5fb004
mariefbj <mariefbjordal@gmail.com>
2017-02-20 19:52:33 +0100

    Added logic to fetch all course codes from IME API

--- utilities/database_filler.py
+++ utilities/database_filler.py
@@ -3,3 +3,15 @@ import json
 
 #base for IME API
 base_url = "http://www.ime.ntnu.no/api/course/en/"
+
+
+#fetch all course codes
+def list_of_codes(base_url):
+    #base_url = "http://www.ime.ntnu.no/api/course/en/"
+    #code = input("Please provide a course code: ")
+    courses = requests.get(base_url+'-').json()
+    codes = []
+    for i in range (len(courses['course'])):
+        code = courses['course'][i]['code']
+        codes.append(code)
+    return codes
