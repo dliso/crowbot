6e5727cbb5975c9fa680584149c95185fcc769d4
Daniel Lisø <daniel.lisoe@gmail.com>
2017-02-22 11:13:26 +0100

    Include jQuery, add an example

--- crowbot/frontend/static/frontend/script.js
+++ crowbot/frontend/static/frontend/script.js
@@ -1,3 +1,14 @@
 // This file is loaded and executed when the main Crowbot page is opened.
 
 console.log('Hello, World');
+
+// jQuery example:
+
+var root = 'https://jsonplaceholder.typicode.com';
+
+$.ajax({
+    url: root + '/posts/1',
+    method: 'GET'
+}).then(function(data) {
+    console.log(data);
+});
