cffbcd35e15ddbfb2c7164fe7e06e42d2dd66781
kristinekf <kristinekf@hotmail.com>
2017-02-22 17:45:19 +0100

    Now the code does the following: When the user writes an input and clicks send, the input is appended to the list and sent to the echoing site. Then the echoing texts is appended to the list (along with "caw caw" hehehe). In addition, I added some more comments in the code.

--- crowbot/frontend/static/frontend/script.js
+++ crowbot/frontend/static/frontend/script.js
@@ -7,7 +7,6 @@ $( document).ready(function(){
     console.log('Hello, World');
 
     // jQuery example:
-
     var root = 'https://jsonplaceholder.typicode.com';
 
     $.ajax({
@@ -18,62 +17,62 @@ $( document).ready(function(){
     });
 
 
-    //Changes color of the list
-    $( "#message-box").css("color","blue");
+    //Changes color of the list:
+    //$( '#message-box').css("color","blue");
 
-    //Adds the word "Hei" to the lists
-    $( "#message-box").append("<li>Hei</li>");
+    //Adds the word "Hei" to the lists:
+    //$( "#message-box").append("<li>Hei</li>");
 
-    var xhttp = new XMLHttpRequest();
-    //scooterlabs.com/echo
-   //// xhttp.open("POST", "http://scooterlabs.com/echo", true);
+    //Creates a new XMLHttpRequest-object but we're not using it. Yet.
+    //var xhttp = new XMLHttpRequest();
 
+    // http://scooterlabs.com/echo is the url of an "echoing site",
+    // but we're not using it and I don't even know if the code works.
+    // xhttp.open("POST", "http://scooterlabs.com/echo", true);
+
+    //Another jQuery example:
     $.ajax({
-        url: root + '/posts',
-        method: "POST",
+        url: root + '/posts', //URL
+        method: "POST", //POST or GET etc...
         data: {
             title: "foo",
             body: "bar",
             userId: 1
         }
         //dataType: "jsonp"
-    }).then(function(data){
+    }).then(function(data){ //"then" waits for the response and executes the function when it arrives.
         console.log(data)
     });
 
 
     //When a user writes in the box and clicks send, the user input is appended to the list
     $( "#message-form" ).submit(function( event ) {
+
+        //Finds user input and appends it
         var input = $( "#user-input").val();
         input = "<li>" + input + "</li>";
         $( "#message-box").append(input);
 
+        //Sends input to URL
         $.ajax({
             url: root + '/posts',
             method: "POST",
             data: {
-
                 body: input
-
             }
-            //dataType: "jsonp"
+        //Gets the input back and appends it to the list
         }).then(function(data){
-            console.log(data)
+            console.log(data);
+            //var data = JSON.parse(data);
+            var output = data.body;
+            console.log(output);
+            $( "#message-box").append("Caw caw " + output);
         });
-
-        //xhttp.send(input);
-        //var echo = xhttp.response();
-        //console.log("The echo" + echo);
-
-
-        //legg til responsen i lista
-
+        //preventDefault prevents the site from updating. I think.
         event.preventDefault();
     });
 
 
 
 
-    $( "#message-box").append($( "#textarea").val());
-
-})
+});
