4471a13dcfe5b853e77eab7cdf49ce6a34aa72b2
mariefbj <mariefbjordal@gmail.com>
2017-03-06 15:50:39 +0100

    Written test for cases where no information is stored in the database Added new test course in test database

--- crowbot/apiai_connection/tests.py
+++ crowbot/apiai_connection/tests.py
@@ -30,6 +30,13 @@ class TestCrowbotChat(TestCase):
                               location='Trondheim',
                               semester='Autumn and Spring',teacher_name='Bjørn-Ove Fimland',teacher_email='bjorn.fimland@ntnu.no',ects_credits=7.50)
 
+#test course with nothing saved in database except required prev knowledge
+        Course.objects.create(code='YRT5678',name='Tulle Emne',
+                              recommended_previous_knowledge='', required_previous_knowledge='YRT6789 Tulle Emne 0,5',
+                              exam_date=None,exam_support_code='',exam_support_name='',
+                              location='',semester='',teacher_name='',
+                              teacher_email='',ects_credits=None)
+
 
 
     def load_text_request_with_query(self, query):
@@ -143,85 +150,155 @@ class TestCrowbotChat(TestCase):
 
 #tester funksjoner i crowbot_chat
 
+    #test for course with credit stored
     def test_user_request_credit(self):
         query = 'How much credit do I get in tdt4140?'
         response = self.load_text_request_with_query(query)
         output = user_request(response)
         self.assertEqual(output,'Credits for TDT4140 Software Engineering is 7.50')
 
+    #test for course with no credit stored
+    def test_user_request_credit_none(self):
+        query = 'How much credit do I get in yrt5678?'
+        response = self.load_text_request_with_query(query)
+        output = user_request(response)
+        self.assertEqual(output,'No information about credits in this course, YRT5678 Tulle Emne')
+
+
+    #test for course with exam date stored
     def test_user_request_exam_date(self):
         query = 'When is the exam in exph0004?'
         response = self.load_text_request_with_query(query)
         output = user_request(response)
         self.assertEqual(output,'Exam date for EXPH0004 Examen philosophicum for Science and Technology is 27/05/2017')
 
-        #exam_date funksjonen i crowbot_chat vil returnere None (ingenting, den går i printen i except)
+    #test for course with no exam date
+    def test_user_request_exam_date_none(self):
         query = 'When is the exam in tdt4140?'
         response = self.load_text_request_with_query(query)
         output = user_request(response)
-        self.assertEqual(output,None)
+        self.assertEqual(output,"No info about the exam in this course TDT4140 Software Engineering")
 
+
+    #test for course with location stored
     def test_user_request_location(self):
         query = 'Where is FE8111 taught?'
         response = self.load_text_request_with_query(query)
         output = user_request(response)
         self.assertEqual(output,'FE8111 Molecular Beam Epitaxy is taught in Trondheim')
 
+    #test for course with no location stored
+    def test_user_request_location_none(self):
+        query = 'Where is YRT5678 taught?'
+        response = self.load_text_request_with_query(query)
+        output = user_request(response)
+        self.assertEqual(output,'No location information for YRT5678 Tulle Emne')
+
+
+    #test for course with professor name stored
     def test_user_request_professor_name(self):
         query = 'Who is teaching FE8111?'
         response = self.load_text_request_with_query(query)
         output = user_request(response)
         self.assertEqual(output,'FE8111 Molecular Beam Epitaxy is taught by Bjørn-Ove Fimland')
 
+    #test for course with no professor name stored
+    def test_user_request_professor_name_none(self):
+        query = 'Who is teaching YRT5678?'
+        response = self.load_text_request_with_query(query)
+        output = user_request(response)
+        self.assertEqual(output,'No information about the lecturer in YRT5678 Tulle Emne')
+
+
+    #test for course with both teacher name and email
     def test_user_request_professor_mail(self):
         query = 'How can I reach the professor in FE8111?'
         response = self.load_text_request_with_query(query)
         output = user_request(response)
         self.assertEqual(output, 'FE8111 Molecular Beam Epitaxy is taught by Bjørn-Ove Fimland. They can be reached at bjorn.fimland@ntnu.no')
 
+    #test for course with no teacher name or teacher email
+    def test_user_request_professor_mail_none(self):
+        query = 'How can I reach the professor in YRT5678?'
+        response = self.load_text_request_with_query(query)
+        output = user_request(response)
+        self.assertEqual(output, 'No information about the professors email in YRT5678 Tulle Emne')
+
+    #TODO: test for course with teacher name, but no teacher email
+
+    #TODO: test for course with no teacher name, but teacher email
+
+
+    #test for course with semester info
     def test_user_request_semester(self):
         query = 'In what term is EXPH0004 taught?'
         response = self.load_text_request_with_query(query)
         output = user_request(response)
         self.assertEqual(output, 'EXPH0004 Examen philosophicum for Science and Technology is taught in the Autumn and Spring')
 
+    #test for course with no semester info
+    def test_user_request_semester_none(self):
+        query = 'In what term is yrt5678 taught?'
+        response = self.load_text_request_with_query(query)
+        output = user_request(response)
+        self.assertEqual(output, 'No information about semesters in YRT5678 Tulle Emne')
+
+
+    #test course with both exam support code and name
     def test_user_request_exam_aids(self):
         query = 'What examination support is permitted in EXPH0004?'
         response = self.load_text_request_with_query(query)
         output = user_request(response)
         self.assertEqual(output, 'Exam support materials for EXPH0004 Examen philosophicum for Science and Technology is code D: No written or handwritten examination support material is permitted. Specified simple calculator is permitted.')
 
-
-        #dette er muligens litt rart, ta med except om tom streng som en return statement?
+    #test course with no exam support code or exam support name
+    def test_user_request_exam_aids_none(self):
         query = 'What exam aids is permitted in TDT4140?'
         response = self.load_text_request_with_query(query)
         output = user_request(response)
-        self.assertEqual(output, 'Exam support materials for TDT4140 Software Engineering is code : ')
+        self.assertEqual(output, 'No information about examination support in TDT4140 Software Engineering')
 
-    #samme her når det er en tom streng, fikse på outputen
+    #TODO: test course with exam support code, but no exam support name
+
+    #TODO: test course with exam support name, but no exam support code
+
+    #test course with needed prev knowledge stored
     def test_user_request_needed_knowledge(self):
+        query = 'What required previous knowledge is there in YRT5678?'
+        response = self.load_text_request_with_query(query)
+        output = user_request(response)
+        self.assertEqual(output, 'Required previous knowledge is: YRT6789 Tulle Emne 0,5')
+
+    #test course with no needed prev knowledge stored
+    def test_user_request_needed_knowledge_none(self):
         query = 'What required previous knowledge is there in FE8111?'
         response = self.load_text_request_with_query(query)
         output = user_request(response)
-        self.assertEqual(output, 'Needed previous knowledge is: ')
+        self.assertEqual(output, 'No information about required previous knowledge in FE8111 Molecular Beam Epitaxy')
 
+
+    #test for course with recommended prev knowledge
     def test_user_request_recommended_knowledge(self):
         query = 'What recommended previous knowledge is there in TDT4140?'
         response = self.load_text_request_with_query(query)
         output = user_request(response)
-        self.assertEqual(output, 'Recommended previous knowledge for TDT4140 Software Engineering is Subjects TDT4100 Object-Oriented Programming and TDT4120 Algorithms and Data Structures, or equivalent.')
+        self.assertEqual(output, 'Recommended previous knowledge for TDT4140 Software Engineering is: Subjects TDT4100 Object-Oriented Programming and TDT4120 Algorithms and Data Structures, or equivalent.')
 
-        #except med tom streng her også
+    #test for course with no recommended prev knowledge
+    def test_user_request_recommended_knowledge_none(self):
         query = 'What recommended previous knowledge is there in EXPH0004?'
         response = self.load_text_request_with_query(query)
         output = user_request(response)
-        self.assertEqual(output, 'Recommended previous knowledge for EXPH0004 Examen philosophicum for Science and Technology is ')
+        self.assertEqual(output, 'No information about recommended previous knowledge for EXPH0004 Examen philosophicum for Science and Technology')
+
 
     def test_user_request_not_valid_coursecode(self):
         query = 'Who is the teacher in TYU7643?'
         response = self.load_text_request_with_query(query)
         output = user_request(response)
         self.assertEqual(output,'No course with code TYU7643')
+
+#tests for ask_apiai in crowbot_chat
 
             #skriv coverage run --source='.' manage.py test myapp
 #i fil directiory hvor manage.py ligger
