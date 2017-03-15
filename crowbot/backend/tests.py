57197816a6d170a64c87d34559e921d19b663de0
mariefbj <mariefbjordal@gmail.com>
2017-03-15 11:40:26 +0100

    Unit tests for backend views. Tester om status koden er 200 der den skal være det og 404 ved en url path som ikke eksisterer. Tester at innholdet i databasen kan leses som json.

--- crowbot/backend/tests.py
+++ crowbot/backend/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
-
+from django.urls import reverse
+import json
 # Create your tests here.
+
+
+class ViewTests(TestCase):
+    def test_index_view_status_code(self):
+        response = self.client.get(reverse('backend:index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_view_response(self):
+        response = self.client.get(reverse('backend:index'))
+        self.assertContains(response,"api index")
+
+    def test_all_courses_view_status_code(self):
+        response = self.client.get(reverse('backend:all_courses'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_all_courses_view_response_json(self):
+        response = self.client.get(reverse('backend:all_courses'))
+        content = response.content
+        response = json.loads(content)
+
+    def test_url_route(self):
+        response = self.client.get('/joik/')
+        self.assertEqual(response.status_code, 404)
+
+
