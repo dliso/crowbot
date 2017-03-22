7f632a4eaa0ba770850f2ab117fbb892208c9d17
Einar Hansen Moheim <Einar Hansen Moheim>
2017-03-22 11:14:49 +0100

    Legger til loginview, og Rating system

--- crowbot/frontend/models.py
+++ crowbot/frontend/models.py
@@ -1,5 +1,9 @@
 from django.db import models
-
+#from __future__ import unicode_literals
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils.encoding import python_2_unicode_compatible
 # Create your models here.
 
 class UserQuery(models.Model):
@@ -7,3 +11,5 @@ class UserQuery(models.Model):
 
     def __str__(self):
         return self.query_text[:50]
+
+
