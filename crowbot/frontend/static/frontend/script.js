79bfb3328130c57e0bcdd72b59e785515ca501d4
Daniel Lisø <daniel.lisoe@gmail.com>
2017-03-24 10:27:54 +0100

    If input is empty, don't do anything

--- crowbot/frontend/static/frontend/script.js
+++ crowbot/frontend/static/frontend/script.js
@@ -108,40 +108,43 @@ $( document).ready(function(){
 
         //Finds user input and appends it
         var input = $( "#user-input").val();
-        //input_html = "<li class='message user-msg'>" + input + "</li>"; // Jeg kommenterte denne ut fordi det virker ikke som den brukes.
-        msgListManager.addTextToList(input, ['user-msg', 'message']);
-        updateScroll(msgBox);
+        // Don't do anything if the input field is empty
+        if (input !== "") {
+            //input_html = "<li class='message user-msg'>" + input + "</li>"; // Jeg kommenterte denne ut fordi det virker ikke som den brukes.
+            msgListManager.addTextToList(input, ['user-msg', 'message']);
+            updateScroll(msgBox);
 
-        root = '/api/ask_question';
+            root = '/api/ask_question';
 
-        //Sends input to URL
-        $.ajax({
-            url: root,
-            method: "POST",
-            data: {
-                body: input
-            }
-        //Gets the input back and appends it to the list
-        }).then(function(data_raw){
-        // We expect to receive either a single message or a list of messages.
-        // If it's a single message, we wrap it in a list so we can treat both
-        // cases the same way.
-            if(Array.isArray(data_raw)) {
-                data = data_raw;
-            } else {
-                data = [data_raw];
-            }
-            for(data of data) {
-                console.log(data);
-                var output = data.body;
-                console.log(data.datetime);
-                message = randomBirdSound() + ' ' + output;
-                //msgListManager.addTextToList(message, ['bot-msg', 'message']);
-                msgListManager.addToListWithTimeAndUser(message, data.usertype, data.username, data.timestamp,['bot-msg', 'message']);
-                updateScroll(msgBox);
-            }
-        });
-        //preventDefault prevents the site from updating. I think.
+            //Sends input to URL
+            $.ajax({
+                url: root,
+                method: "POST",
+                data: {
+                    body: input
+                }
+                //Gets the input back and appends it to the list
+            }).then(function(data_raw){
+                // We expect to receive either a single message or a list of messages.
+                // If it's a single message, we wrap it in a list so we can treat both
+                // cases the same way.
+                if(Array.isArray(data_raw)) {
+                    data = data_raw;
+                } else {
+                    data = [data_raw];
+                }
+                for(data of data) {
+                    console.log(data);
+                    var output = data.body;
+                    console.log(data.datetime);
+                    message = randomBirdSound() + ' ' + output;
+                    //msgListManager.addTextToList(message, ['bot-msg', 'message']);
+                    msgListManager.addToListWithTimeAndUser(message, data.usertype, data.username, data.timestamp,['bot-msg', 'message']);
+                    updateScroll(msgBox);
+                }
+            });
+            //preventDefault prevents the site from updating. I think.
+        }
         event.preventDefault();
     });
 
