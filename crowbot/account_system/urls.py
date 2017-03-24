84ad086d3a06903f93ea638b02204bd31d8f07d3
Daniel Lisø <daniel.lisoe@gmail.com>
2017-03-24 12:21:23 +0100

    Fix logout route so it goes to the main page

--- crowbot/account_system/urls.py
+++ crowbot/account_system/urls.py
@@ -7,6 +7,6 @@ from . import views as core_views
 urlpatterns = [
     url(r'^$|^profile/$', core_views.home, name='home'),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': 'index'}, name='logout'),
+    url(r'^logout/$', auth_views.logout, {'next_page': 'frontend:index'}, name='logout'),
     url(r'^signup/$', core_views.signup, name='signup'),
 ]
