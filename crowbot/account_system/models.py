3297907a7bb0eedd6f71e2e9aeeb64cb98575b20
Daniel Lisø <daniel.lisoe@gmail.com>
2017-04-03 09:33:34 +0200

    Add subscribed_courses field to Profile model

--- crowbot/account_system/models.py
+++ crowbot/account_system/models.py
@@ -4,6 +4,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
+from backend import models as backend_models
 
 
 # Create your models here.
@@ -21,9 +22,10 @@ class Profile(models.Model):
     institute = models.CharField(max_length=30, blank=True)
     birthdate = models.DateField(null=True, blank=True)
     role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, null=True, blank=True)
+    subscribed_courses = models.ManyToManyField(backend_models.Course)
 
-def __str__(self):
-        return self.user.username
+    def __str__(self):
+            return self.user.username
 
 
 @receiver(post_save, sender=User)
