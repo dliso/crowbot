bc9ced9d3dfae2bf2d2bb33cc2a73875cc0c6bdb
Daniel Lisø <daniel.lisoe@gmail.com>
2017-04-03 12:56:15 +0200

    Expose backend models via admin interface

--- crowbot/backend/admin.py
+++ crowbot/backend/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import *
+
+admin.site.register(Course)
+admin.site.register(Book)
+admin.site.register(Room)
+admin.site.register(Semester)
+admin.site.register(Question)
+admin.site.register(Answer)
+
 # Register your models here.
